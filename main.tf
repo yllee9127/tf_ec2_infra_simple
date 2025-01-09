9594e8f1825fffa27e6d45850553efbdcc77e1d9
yllee9127 <yllee9127@gmail.com>
2025-01-09 09:49:03 +0800

    Updated VPC

--- main.tf
+++ main.tf
@@ -5,11 +5,11 @@ module "vpc-1" {
 
   cidr             = "10.1.0.0/16"
   azs              = slice(data.aws_availability_zones.available.names, 0, 3)
-  private_subnets  = ["10.1.1.0/24", "10.1.2.0/24", "10.1.3.0/24"]
+  #private_subnets  = ["10.1.1.0/24", "10.1.2.0/24", "10.1.3.0/24"]
   public_subnets   = ["10.1.101.0/24", "10.1.102.0/24", "10.1.103.0/24"]
   # database_subnets = ["10.0.201.0/24", "10.0.202.0/24", "10.0.203.0/24"]
 
-  enable_nat_gateway   = true  # set to false if no private subnet
+  enable_nat_gateway   = false  # set to false if no private subnet
   single_nat_gateway   = false
   enable_dns_hostnames = true # needed for DNS resolution
 }
